3171edb33715258a74d3433e3c0413e4edfac7a9
Maicon Gabriel Friedel <maicon.friedel@kyly.com.br>
2020-01-14 13:04:59 -0300

    Initial Commit
    - Implementado backend da aplicação Semana Omnistack 10.0

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/omnistack", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("connected");
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
