3f5035ba9b84dde32324d1c141b3bae612c1edf0
Maicon Gabriel Friedel <maicon.friedel@gmail.com>
2020-01-15 19:22:52 -0300

    Added Cors

--- src/server.js
+++ src/server.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/omnistack", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  "mongodb+srv://omnistack10:omnistack10@cluster0-81scl.mongodb.net/omnistack10",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 mongoose.connection.on("connected", () => {
   console.log("connected");
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
