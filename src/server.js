27b3c05e9d11a2e92e03322f76fa417d97e8ef65
Maicon Gabriel Friedel <maicon.friedel@kyly.com.br>
2020-01-17 13:20:57 -0300

    Started Video 5
    - Started video 5 with the websocket connection

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes");
 const cors = require("cors");
+const http = require("http");
+const routes = require("./routes");
+
+const { setUpWebSocket } = require("./websocket");
 
 const app = express();
+const server = http.Server(app);
 
-mongoose.connect(
-  "mongodb+srv://omnistack10:omnistack10@cluster0-81scl.mongodb.net/omnistack10",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+setUpWebSocket(server);
+
+mongoose.connect("mongodb://localhost:27017/omnistack10", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 mongoose.connection.on("connected", () => {
   console.log("connected");
@@ -21,4 +24,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
