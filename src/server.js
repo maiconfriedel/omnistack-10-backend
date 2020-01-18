83dccfdb6b0524f7070ca8b4ca444935c843c93a
Maicon Gabriel Friedel <maicon.friedel@gmail.com>
2020-01-17 22:14:38 -0300

    Finished Last Video with Websocket features

--- src/server.js
+++ src/server.js
@@ -11,10 +11,13 @@ const server = http.Server(app);
 
 setUpWebSocket(server);
 
-mongoose.connect("mongodb://localhost:27017/omnistack10", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  "mongodb+srv://omnistack10:omnistack10@cluster0-81scl.mongodb.net/omnistack10",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 mongoose.connection.on("connected", () => {
   console.log("connected");
--- src/websocket.js
+++ src/websocket.js
@@ -1,9 +1,37 @@
 const socketio = require("socket.io");
+const parseStringAsArray = require("./utils/parseStringAsArray");
+const calculateDistance = require("./utils/calculateDistance");
+const connections = [];
+let io;
 
 exports.setUpWebSocket = server => {
-  const io = socketio(server);
+  io = socketio(server);
 
   io.on("connection", socket => {
-    console.log(socket.id);
+    const { latitude, longitude, techs } = socket.handshake.query;
+
+    connections.push({
+      id: socket.id,
+      coordinates: {
+        latitude: Number(latitude),
+        longitude: Number(longitude)
+      },
+      techs: parseStringAsArray(techs)
+    });
+  });
+};
+
+exports.findConnections = (coordinates, techs) => {
+  return connections.filter(conn => {
+    return (
+      calculateDistance(coordinates, conn.coordinates) < 10 &&
+      conn.techs.some(item => techs.includes(item))
+    );
+  });
+};
+
+exports.sendMessage = (to, message, data) => {
+  to.forEach(conn => {
+    io.to(conn.id).emit(message, data);
   });
 };
